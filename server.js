f4b7e3697e550ed13609c81e450a108145df766d
Monica Scroggs <scrmr0210@gmail.com>
2020-02-13 16:42:32 -0500

    fixed scraper so it loads correct summary io

--- server.js
+++ server.js
@@ -10,8 +10,15 @@ var app = express();
 axios.get("https://www.economist.com/latest/").then(function (response, html) {
     var $ = cheerio.load(response.data);
     $("article").each(function(i, element) {
-        var article = $(element).children("a").attr("aria-label");
-        console.log(article);
+        var title = $(element).children("a").attr("aria-label");
+        var link = $(element).children("a").attr("href");
+        var summary = $(element).children("a").children("div").last().find("div.teaser__text").text();
+
+
+        console.log(title);
+        console.log(link);
+        console.log(summary);
+
     })
 });
 
